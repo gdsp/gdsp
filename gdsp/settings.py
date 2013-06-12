1ff3afdac0044a3999d25b9fa9fdf92bd64f8bcf
Anders Wold Eldhuset <andereld@samfundet.no>
2013-06-12 16:25:13 +0200

    Add South and the beginnings of a requirements.txt
    Keeping dependencies and database changes under control is a good thing.

--- gdsp/settings.py
+++ gdsp/settings.py
@@ -119,10 +119,8 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'south',
 )
 
 # In production, make sure that the settings_prod.py file is present
