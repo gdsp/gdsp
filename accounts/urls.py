a1cefc16785ad6599557415ee843c3101330dfe8
Anders Wold Eldhuset <andereld@samfundet.no>
2013-07-09 02:35:38 +0200

    Add user registration

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import auth
 
+from views import RegisterView
+
 urlpatterns = patterns('',
         url(r'^login/$', 'django.contrib.auth.views.login',
             kwargs={'template_name': 'accounts/login.html'},
@@ -10,4 +12,5 @@ urlpatterns = patterns('',
             kwargs={'next_page': '/lessons/'},
             name='logout',
         ),
+        url(r'^register/$', RegisterView.as_view(), name='register'),
 )
--- accounts/views.py
+++ accounts/views.py
@@ -1,1 +1,25 @@
-# Create your views here.
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.views.generic.base import TemplateView
+
+class RegisterView(TemplateView):
+    template_name = 'accounts/register.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        super(RegisterView, self).dispatch(request, *args, **kwargs)
+        if request.method == 'POST':
+            form = UserCreationForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = authenticate(
+                        username=form.cleaned_data['username'],
+                        password=form.cleaned_data['password1'],
+                )
+                login(request, user)
+                return HttpResponseRedirect(reverse('core:lessons'))
+        else:
+            form = UserCreationForm()
+        return render(request, self.__class__.template_name, {'form': form})
