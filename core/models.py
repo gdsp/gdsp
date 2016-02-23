fa1e86accdf38eb5cd6878c28b0752459d7da87c
ssaue <sigurd.saue@gmail.com>
2016-02-23 22:56:39 +0100

    Added default test feedback

--- core/models.py
+++ core/models.py
@@ -225,13 +225,13 @@ class TestElement(BaseTopicElement):
                                     choices=tuple([ (fx,fx) for fx in find_inc_files() + ['ALL FX', 'Effects the student has seen so far' ]]))
 
     feedback_bad = models.TextField(
-            help_text=_('Feedback for a badly performing student. He/she should be practicing more, perhaps?'), default=''
+            help_text=_('Feedback for a badly performing student. He/she should be practicing more, perhaps?'), default='Keep on working!'
     )
     feecback_ok =  models.TextField(
-            help_text=_('Feedback for a student that is doing OK.'), default=''
+            help_text=_('Feedback for a student that is doing OK.'), default='Well done!'
     )
     feecback_good =  models.TextField(
-            help_text=_('Feedback for a student that is doing very good, maybe he/she should be encouraged to move on to the next lesson?'), default=''
+            help_text=_('Feedback for a student that is doing very good, maybe he/she should be encouraged to move on to the next lesson?'), default='Brilliant. You can move on!'
     )
 
     def save(self, *args, **kwargs):
