6b4e0883d94c63ef496da66bc14938d3a1faeace
Morten Kleveland <mortengkleveland@gmail.com>
2016-03-07 17:13:21 +0100

    Add play pause button

--- static/javascript/interactive_csound.js
+++ static/javascript/interactive_csound.js
@@ -11,7 +11,7 @@ function moduleDidLoad() {
 }
 
 function attachListeners() {
-    //document.getElementById("playPauseButton").addEventListener("click", play);
+    document.getElementById("playPauseButton").addEventListener("click", play);
 }
 
 // function handleMessage(message) {
@@ -32,6 +32,15 @@ function attachListeners() {
 // }
 
 function play() {
+    if (isPlaying) {
+        csound.Event("i-1 0 -1");
+        csound.Event("i-2 0 -1");
+        document.getElementById("playPauseButton").src = "/static/images/play.png";
+    } else {
+        csound.Event("i1 0 -1");
+        csound.Event("i2 0 -1");
+        document.getElementById("playPauseButton").src = "/static/images/pause.png";
+    }
     isPlaying = !isPlaying
 }
 
